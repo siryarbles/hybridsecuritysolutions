f81c8650484c6c223d658c2a8c6ebe485656ee5d
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2018-08-13 22:06:36 -0400

    started adding session tracking

--- src/LogoutServlet.java
+++ src/LogoutServlet.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -17,19 +18,20 @@ public class LogoutServlet extends HttpServlet {
        
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	response.setContentType("text/html");
-    	Cookie loginCookie = null;
     	Cookie[] cookies = request.getCookies();
     	if(cookies != null){
     	for(Cookie cookie : cookies){
-    		if(cookie.getName().equals("user")){
-    			loginCookie = cookie;
+    		if(cookie.getName().equals("JSESSIONID")){
+    			System.out.println("JSESSIONID="+cookie.getValue());
     			break;
     		}
     	}
     	}
-    	if(loginCookie != null){
-    		loginCookie.setMaxAge(0);
-        	response.addCookie(loginCookie);
+    	//invalidate the session if exists
+    	HttpSession session = request.getSession(false);
+    	System.out.println("User="+session.getAttribute("user"));
+    	if(session != null){
+    		session.invalidate();
     	}
     	response.sendRedirect("login.html");
     }
